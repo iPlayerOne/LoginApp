512e8d74bc26164d3b0c3e4ce87b3ef299dc4540
Ivan Korobov <ivan.koroboff@gmail.com>
2022-07-06 22:32:37 +0200

    чуть чуть еще

---
+++ LoginApp/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  LoginApp
+//
+//  Created by ikorobov on 06.07.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithDefaultBackground()
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+    }
+    
+}
